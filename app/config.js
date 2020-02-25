ab79badd381e51b570503a9d7900376c71402a0c
kat1906 <katie.armstrong@bjss.com>
2020-02-25 16:32:22 +0000

    initial commit - set up app

---
+++ app/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // App name
+  appName: 'NHSD Buying Catalogue Admin',
+
+  // Environment
+  env: process.env.NODE_ENV || 'development',
+
+  // Port to run local development server on
+  port: process.env.PORT || 3002,
+
+  // API_HOST
+  apiHost: process.env.API_HOST || 'http://localhost:8080',
+
+  // DOCUMENT_HOST
+  documentHost: process.env.DOCUMENT_HOST || 'http://localhost:8090',
+
+  // LOGGER_LEVEL options are info, warn, error, off
+  loggerLevel: process.env.LOGGER_LEVEL || 'error',
+};
