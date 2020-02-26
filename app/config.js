67f431f8d520591dd8cfcc9f2293b9b3703d4846
kat1906 <katie.armstrong@bjss.com>
2020-02-26 08:58:36 +0000

    remove document host env variable

--- app/config.js
+++ app/config.js
@@ -11,9 +11,6 @@ module.exports = {
   // API_HOST
   apiHost: process.env.API_HOST || 'http://localhost:8080',
 
-  // DOCUMENT_HOST
-  documentHost: process.env.DOCUMENT_HOST || 'http://localhost:8090',
-
   // LOGGER_LEVEL options are info, warn, error, off
   loggerLevel: process.env.LOGGER_LEVEL || 'error',
 };
