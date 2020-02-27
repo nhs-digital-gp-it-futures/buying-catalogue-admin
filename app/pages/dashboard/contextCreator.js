a3be4c7f74c4c005383a23b46926d1b1d08041b3
kat1906 <katie.armstrong@bjss.com>
2020-02-27 14:05:15 +0000

    tidy up

--- app/pages/dashboard/contextCreator.js
+++ app/pages/dashboard/contextCreator.js
@@ -2,5 +2,9 @@ import manifest from './manifest.json';
 
 export const getOrgDashboardContext = data => ({
   ...manifest,
+  columnInfo: manifest.columnInfo ? manifest.columnInfo.map(heading => ({
+    data: heading.data || '',
+    link: heading.link || false,
+  })) : [],
   data: data ? data.map(row => [row.name || '', row.odsCode || '']) : [],
 });
