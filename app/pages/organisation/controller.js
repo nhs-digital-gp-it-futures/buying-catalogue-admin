4a8fdb56fbee1a0c0e44fe38655ff10b3aff5b44
kat1906 <katie.armstrong@bjss.com>
2020-02-28 16:28:44 +0000

    add controller for get organisation accounts

---
+++ app/pages/organisation/controller.js
@@ -0,0 +1,27 @@
+// import axios from 'axios';
+import { getContext } from './contextCreator'
+import { identityServer } from '../../config';
+import logger from '../../logger';
+
+export const getOrgAccountsContext = (orgId) => {
+  const endpoint = `${identityServer}/api/v1/Organisations/${orgId}`;
+  logger.info(`api called: [GET] ${endpoint}`);
+  logger.info(`Organisation ${orgId} returned`);
+  return getContext();
+};
+
+// TODO: Uncomment when API work is done and remove the implementation above
+// export const getOrgAccountsContext = async (orgId) => {
+//   const endpoint = `${identityServer}/api/v1/Organisations/${orgId}`;
+//   logger.info(`api called: [GET] ${endpoint}`);
+//   const response = await axios.get(endpoint);
+
+//   if (response.data) {
+//     logger.info(`Organisation ${orgId} returned`);
+//     const { data } = response;
+//     return getContext({ data });
+//     return getContext();
+//   }
+
+//   throw new Error('No data returned');
+// };
