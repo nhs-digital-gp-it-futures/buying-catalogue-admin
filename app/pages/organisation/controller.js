d5bef40573608af18865808feb83bd772f9c41c7
kat1906 <katie.armstrong@bjss.com>
2020-03-02 10:03:32 +0000

    make controller work for no data

--- app/pages/organisation/controller.js
+++ app/pages/organisation/controller.js
@@ -7,7 +7,7 @@ export const getOrgAccountsContext = (orgId) => {
   const endpoint = `${identityServer}/api/v1/Organisations/${orgId}`;
   logger.info(`api called: [GET] ${endpoint}`);
   logger.info(`Organisation ${orgId} returned`);
-  return getContext();
+  return getContext({ data: {} });
 };
 
 // TODO: Uncomment when API work is done and remove the implementation above
@@ -20,7 +20,6 @@ export const getOrgAccountsContext = (orgId) => {
 //     logger.info(`Organisation ${orgId} returned`);
 //     const { data } = response;
 //     return getContext({ data });
-//     return getContext();
 //   }
 
 //   throw new Error('No data returned');
