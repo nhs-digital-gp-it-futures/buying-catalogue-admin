759f4af8abcc8afda5e50484fd0ae1d51cdadb08
kat1906 <katie.armstrong@bjss.com>
2020-03-03 13:28:37 +0000

    add adduser context creator

---
+++ app/pages/adduser/contextCreator.test.js
@@ -0,0 +1,33 @@
+import manifest from './manifest.json';
+import { getContext } from './contextCreator';
+
+const mockData = {
+  organisationId: 'org1',
+  name: 'Manchester',
+};
+
+describe('getContext', () => {
+  it('should return the contents of the manfest', () => {
+    const context = getContext({});
+    expect(context.title).toEqual(manifest.title);
+    expect(context.description).toEqual(manifest.description);
+    expect(context.orgNameSubheading).toEqual(manifest.orgNameSubheading);
+    expect(context.questions.length).toEqual(manifest.questions.length);
+    context.questions.forEach((question, i) => {
+      expect(question.id).toEqual(manifest.questions[i].id);
+      expect(question.text).toEqual(manifest.questions[i].text);
+    });
+    expect(context.addUserButtonText).toEqual(manifest.addUserButtonText);
+  });
+
+  it('should add orgId and orgName from the data provided data provided', () => {
+    const context = getContext(mockData);
+    expect(context.orgId).toEqual(mockData.organisationId);
+    expect(context.orgName).toEqual(mockData.name);
+  });
+
+  it('should add submitActionUrl from the provided organisation id', () => {
+    const context = getContext(mockData);
+    expect(context.submitActionUrl).toEqual(`/organisations/${mockData.organisationId}/adduser`);
+  });
+});
