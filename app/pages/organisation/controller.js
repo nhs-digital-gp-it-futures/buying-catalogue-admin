46f9cb70a800f492510cea9ac3a87de3d1f31b96
kat1906 <katie.armstrong@bjss.com>
2020-03-18 12:10:27 +0000

    change data to organisation

--- app/pages/organisation/controller.js
+++ app/pages/organisation/controller.js
@@ -4,15 +4,15 @@ import { logger } from '../../logger';
 
 export const getOrgAccountsContext = async ({ organisationId, accessToken }) => {
   const options = { organisationId };
-  const orgData = await getData({ endpointLocator: 'getOrgById', options, accessToken });
+  const organisation = await getData({ endpointLocator: 'getOrgById', options, accessToken });
 
-  if (orgData) {
-    logger.info(`Organisation ${orgData.name} (${organisationId}) returned`);
+  if (organisation) {
+    logger.info(`Organisation ${organisation.name} (${organisationId}) returned`);
     const users = await getData({ endpointLocator: 'getUsers', options, accessToken });
 
     logger.info(`${users.users ? users.users.length : 'No'} users found`);
-    orgData.users = users.users;
-    return getContext({ organisation: orgData });
+    organisation.users = users.users;
+    return getContext({ organisation });
   }
 
   throw new Error(`No organisation data returned for id: ${organisationId}`);
