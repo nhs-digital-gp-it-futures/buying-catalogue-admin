33c0594468f4f61f7656e51fa34844328f8a2ccd
kat1906 <katie.armstrong@bjss.com>
2020-03-19 15:36:29 +0000

    bring in line with PB and MP

--- app/helpers/endpoints.js
+++
@@ -1,7 +0,0 @@
-import { organisationApiUrl } from '../config';
-
-export const endpoints = {
-  getOrganisations: () => `${organisationApiUrl}/api/v1/Organisations`,
-  getOrgById: options => `${organisationApiUrl}/api/v1/Organisations/${options.organisationId}`,
-  getUsers: options => `${organisationApiUrl}/api/v1/Organisations/${options.organisationId}/users`,
-};
