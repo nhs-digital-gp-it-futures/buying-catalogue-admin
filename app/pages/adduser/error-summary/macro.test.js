1ebebf2ddbadf2ec86b65b7f45d5bdeecc4ca238
kat1906 <katie.armstrong@bjss.com>
2020-03-25 16:05:06 +0000

    add error summary component

--- app/pages/adduser/error-summary/macro.test.js
+++ app/pages/adduser/error-summary/macro.test.js
@@ -3,7 +3,7 @@ import { createTestHarness } from '../../../test-utils/testHarness';
 const setup = {
   component: {
     name: 'errorSummary',
-    path: 'common/components/error-summary/macro.njk',
+    path: 'pages/adduser/error-summary/macro.njk',
   },
 };
 
