18a3ca3c937cf560ccb600ec0920183e2e42157c
kat1906 <katie.armstrong@bjss.com>
2020-03-25 15:58:30 +0000

    add getErrorContext

--- app/pages/adduser/contextCreator.js
+++ app/pages/adduser/contextCreator.js
@@ -1,9 +1,63 @@
 import manifest from './manifest.json';
 import { baseUrl } from '../../config';
+import { formatErrors, formatAllErrors, addErrorsToManifest } from './contextCreatorErrorsHelper';
 
-export const getContext = data => ({
+export const getContext = orgData => ({
   ...manifest,
-  organisationId: data.organisationId,
-  organisationName: data.name,
-  backLinkHref: `${baseUrl}/organisations/${data.organisationId}`,
+  organisationId: orgData.organisationId,
+  organisationName: orgData.name,
+  backLinkHref: `${baseUrl}/organisations/${orgData.organisationId}`,
 });
+
+const validationErrorsDefault = [
+  {
+    field: 'firstName',
+    id: 'FirstNameRequired',
+  },
+  {
+    field: 'firstName',
+    id: 'FirstNameTooLong',
+  },
+  {
+    field: 'lastName',
+    id: 'LastNameRequired',
+  },
+  {
+    field: 'lastName',
+    id: 'LastNameTooLong',
+  },
+  {
+    field: 'emailAddress',
+    id: 'EmailRequired',
+  },
+  {
+    field: 'emailAddress',
+    id: 'EmailTooLong',
+  },
+  {
+    field: 'emailAddress',
+    id: 'EmailInvalidFormat',
+  },
+  {
+    field: 'emailAddress',
+    id: 'EmailAlreadyExists',
+  },
+  {
+    field: 'phoneNumber',
+    id: 'PhoneNumberRequired',
+  },
+];
+
+export const getErrorContext = ({ orgData, validationErrors = validationErrorsDefault }) => {
+  const formattedErrors = formatErrors(validationErrors);
+  const allErrors = formatAllErrors(formattedErrors);
+  const manifestWithErrors = addErrorsToManifest({ manifest, errors: formattedErrors });
+
+  return {
+    ...manifestWithErrors,
+    organisationId: orgData.organisationId,
+    organisationName: orgData.name,
+    errors: allErrors,
+    backLinkHref: `${baseUrl}/organisations/${orgData.organisationId}`,
+  };
+};
--- app/pages/adduser/contextCreator.test.js
+++ app/pages/adduser/contextCreator.test.js
@@ -1,33 +1,82 @@
 import manifest from './manifest.json';
-import { getContext } from './contextCreator';
+import { getContext, getErrorContext } from './contextCreator';
+import { errorMessages } from './errorStrings';
 
 const mockData = {
   organisationId: 'org1',
   name: 'Manchester',
 };
 
-describe('getContext', () => {
-  it('should return the contents of the manfest', () => {
-    const context = getContext({});
-    expect(context.title).toEqual(manifest.title);
-    expect(context.description).toEqual(manifest.description);
-    expect(context.orgNameSubheading).toEqual(manifest.orgNameSubheading);
-    expect(context.questions.length).toEqual(manifest.questions.length);
-    context.questions.forEach((question, i) => {
-      expect(question.id).toEqual(manifest.questions[i].id);
-      expect(question.text).toEqual(manifest.questions[i].text);
+const mockValidationErrors = [{
+  field: 'firstName',
+  id: 'FirstNameRequired',
+},
+{
+  field: 'firstName',
+  id: 'FirstNameTooLong',
+}];
+
+describe('adduser contextCreator', () => {
+  describe('getContext', () => {
+    it('should return the contents of the manifest', () => {
+      const context = getContext({});
+      expect(context.title).toEqual(manifest.title);
+      expect(context.description).toEqual(manifest.description);
+      expect(context.orgNameSubheading).toEqual(manifest.orgNameSubheading);
+      expect(context.questions.length).toEqual(manifest.questions.length);
+      context.questions.forEach((question, i) => {
+        expect(question.id).toEqual(manifest.questions[i].id);
+        expect(question.text).toEqual(manifest.questions[i].text);
+      });
+      expect(context.addUserButtonText).toEqual(manifest.addUserButtonText);
     });
-    expect(context.addUserButtonText).toEqual(manifest.addUserButtonText);
-  });
 
-  it('should add organisationId and organisationName from the data provided', () => {
-    const context = getContext(mockData);
-    expect(context.organisationId).toEqual(mockData.organisationId);
-    expect(context.organisationName).toEqual(mockData.name);
-  });
+    it('should add organisationId and organisationName from the data provided', () => {
+      const context = getContext(mockData);
+      expect(context.organisationId).toEqual(mockData.organisationId);
+      expect(context.organisationName).toEqual(mockData.name);
+    });
 
-  it('should construct backLinkHref from the data provided', () => {
-    const context = getContext(mockData);
-    expect(context.backLinkHref).toEqual(`/organisations/${mockData.organisationId}`);
+    it('should construct backLinkHref from the data provided', () => {
+      const context = getContext(mockData);
+      expect(context.backLinkHref).toEqual(`/organisations/${mockData.organisationId}`);
+    });
+  });
+  describe('getErrorContext', () => {
+    it('should return the contents of the manifest', () => {
+      const context = getErrorContext({ orgData: {}, validationErrors: [] });
+      expect(context.title).toEqual(manifest.title);
+      expect(context.description).toEqual(manifest.description);
+      expect(context.orgNameSubheading).toEqual(manifest.orgNameSubheading);
+      expect(context.questions.length).toEqual(manifest.questions.length);
+      context.questions.forEach((question, i) => {
+        expect(question.id).toEqual(manifest.questions[i].id);
+        expect(question.text).toEqual(manifest.questions[i].text);
+      });
+      expect(context.addUserButtonText).toEqual(manifest.addUserButtonText);
+    });
+
+    it('should add organisationId and organisationName from the data provided', () => {
+      const context = getErrorContext({ orgData: mockData, validationErrors: [] });
+      expect(context.organisationId).toEqual(mockData.organisationId);
+      expect(context.organisationName).toEqual(mockData.name);
+    });
+
+    it('should construct backLinkHref from the data provided', () => {
+      const context = getErrorContext({ orgData: mockData, validationErrors: [] });
+      expect(context.backLinkHref).toEqual(`/organisations/${mockData.organisationId}`);
+    });
+
+    it('should construct errors array from the data provided', () => {
+      const context = getErrorContext({
+        orgData: mockData,
+        validationErrors: mockValidationErrors,
+      });
+      expect(context.errors.length).toEqual(mockValidationErrors.length);
+      context.errors.forEach((error, i) => {
+        expect(error.href).toEqual(`#${mockValidationErrors[i].field.toLowerCase()}`);
+        expect(error.text).toEqual(errorMessages[mockValidationErrors[i].id]);
+      });
+    });
   });
 });
