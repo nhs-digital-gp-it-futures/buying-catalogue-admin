9f3574aa580d861430e040b0b2970ef0afbce46b
kat1906 <katie.armstrong@bjss.com>
2020-04-01 13:48:45 +0100

    extract format address to helper function

--- app/pages/editorg/contextCreator.js
+++ app/pages/editorg/contextCreator.js
@@ -1,13 +1,13 @@
 import manifest from './manifest.json';
 import { baseUrl } from '../../config';
-import { transformObjectValuesToArray } from '../../helpers/contextCreatorHelper';
+import { extractObjectValuesToArray } from '../../helpers/contextCreatorHelper';
 
 export const getContext = ({ organisation }) => ({
   ...manifest,
   organisationName: organisation.name,
   odsCode: organisation.odsCode,
   primaryRoleId: organisation.primaryRoleId,
-  address: organisation.address ? transformObjectValuesToArray(organisation.address) : [],
+  address: organisation.address ? extractObjectValuesToArray(organisation.address) : [],
   agreementSigned: organisation.catalogueAgreementSigned,
   backLinkHref: `${baseUrl}/organisations/${organisation.organisationId}`,
   saveCatalogueAgreementPostAction: `${baseUrl}/organisations/${organisation.organisationId}`,
