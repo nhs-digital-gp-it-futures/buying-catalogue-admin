a4a3a47e09a1d89a26dd3031fc24a961c6e351f3
Fawad Ali <alibombaye@gmail.com>
2020-04-03 13:06:26 +0100

    removed get live

--- app/pages/health/live/getLiveStatus.js
+++
@@ -1,3 +0,0 @@
-import { status } from '../status';
-
-export const getLiveStatus = () => status.healthy;
--- app/pages/health/live/getLiveStatus.test.js
+++
@@ -1,9 +0,0 @@
-import { status } from '../status';
-import { getLiveStatus } from './getLiveStatus';
-
-describe('getLiveStatus', () => {
-  it('should return \'Healthy\' when /health/live is called', () => {
-    const liveStatus = getLiveStatus();
-    expect(liveStatus).toBe(status.healthy);
-  });
-});
