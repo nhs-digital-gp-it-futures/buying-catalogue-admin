075bc069eec8382bcf2bb8d0e0f23b630d4f3131
kat1906 <katie.armstrong@bjss.com>
2020-04-03 10:32:11 +0100

    move addParamsToManifest to contextCreatorHelper

--- app/helpers/contextCreatorHelper.js
+++ app/helpers/contextCreatorHelper.js
@@ -2,3 +2,9 @@ export const extractObjectValuesToArray = obj => Object.values(obj).reduce((acc,
   if (value) acc.push(value);
   return acc;
 }, []);
+
+export const addParamsToManifest = (json, params) => JSON.parse(
+  Object.entries(params).reduce(
+    (string, [key, value]) => string.replace(new RegExp(`{{${key}}}`, 'g'), value), JSON.stringify(json),
+  ),
+);
