9ceb3b9799da6f20c82460869a8e85f421d71817
kat1906 <katie.armstrong@bjss.com>
2020-04-08 12:40:18 +0100

    link up add org button

--- app/pages/dashboard/template.test.js
+++ app/pages/dashboard/template.test.js
@@ -1,11 +1,36 @@
 import { createTestHarness } from '../../test-utils/testHarness';
-import context from './manifest.json';
+import content from './manifest.json';
 import { publicBrowseBaseUrl } from '../../config';
 
 const setup = {
   template: {
     path: 'pages/dashboard/template.njk',
   },
+};
+
+const context = {
+  ...content,
+  organisations: [
+    [{
+      data: 'A lovely organisation',
+      href: '/organisations/org1',
+      dataTestId: 'org-name-org1',
+    },
+    {
+      data: 'ODS1',
+      dataTestId: 'ods-code-org1',
+    }],
+    [{
+      data: 'Another lovely organisation',
+      href: '/organisations/org2',
+      dataTestId: 'org-name-org2',
+    },
+    {
+      data: 'ODS2',
+      dataTestId: 'ods-code-org2',
+    }],
+  ],
+  addOrgButtonHref: '/organisations/addorganisation',
 };
 
 describe('organisations dashboard page', () => {
@@ -22,7 +47,7 @@ describe('organisations dashboard page', () => {
     harness.request(context, ($) => {
       const title = $('h1[data-test-id="org-dashboard-title"]');
       expect(title.length).toEqual(1);
-      expect(title.text().trim()).toEqual(context.title);
+      expect(title.text().trim()).toEqual(content.title);
     });
   }));
 
@@ -30,7 +55,7 @@ describe('organisations dashboard page', () => {
     harness.request(context, ($) => {
       const description = $('[data-test-id="org-dashboard-description"]');
       expect(description.length).toEqual(1);
-      expect(description.text().trim()).toEqual(context.description);
+      expect(description.text().trim()).toEqual(content.description);
     });
   }));
 
@@ -38,19 +63,42 @@ describe('organisations dashboard page', () => {
     harness.request(context, ($) => {
       const addOrgButton = $('[data-test-id="add-org-button"] a');
       expect(addOrgButton.length).toEqual(1);
-      expect(addOrgButton.text().trim()).toEqual(context.addOrgButtonText);
-      expect(addOrgButton.attr('href')).toEqual('#');
+      expect(addOrgButton.text().trim()).toEqual(content.addOrgButtonText);
+      expect(addOrgButton.attr('href')).toEqual(context.addOrgButtonHref);
     });
   }));
 
-  it('should render the table component', createTestHarness(setup, (harness) => {
+  it('should render the table component with data', createTestHarness(setup, (harness) => {
     harness.request(context, ($) => {
       const orgTable = $('[data-test-id="org-table"]');
+      const columnHeading1 = orgTable.find('[data-test-id="column-heading-0"]');
+      const columnHeading2 = orgTable.find('[data-test-id="column-heading-1"]');
+      const row1 = $('[data-test-id="table-row-0"]');
+      const row1OrgName = row1.find('a[data-test-id="org-name-org1"]');
+      const row1OdsCode = row1.find('div[data-test-id="ods-code-org1"]');
+      const row2 = $('[data-test-id="table-row-1"]');
+      const row2OrgName = row2.find('a[data-test-id="org-name-org2"]');
+      const row2OdsCode = row2.find('div[data-test-id="ods-code-org2"]');
+
       expect(orgTable.length).toEqual(1);
       expect(orgTable.find('[data-test-id="table"]').length).toEqual(1);
       expect(orgTable.find('[data-test-id="table-headings"]').length).toEqual(1);
-      expect(orgTable.find('[data-test-id="column-heading"]').length).toEqual(2);
-      // TODO: Add tests to check the organisations data comes through
+      expect(columnHeading1.length).toEqual(1);
+      expect(columnHeading1.text().trim()).toEqual(content.columnInfo[0].data);
+      expect(columnHeading2.length).toEqual(1);
+      expect(columnHeading2.text().trim()).toEqual(content.columnInfo[1].data);
+      expect(row1.length).toEqual(1);
+      expect(row1OrgName.length).toEqual(1);
+      expect(row1OrgName.text().trim()).toEqual(context.organisations[0][0].data);
+      expect(row1OrgName.attr('href')).toEqual(context.organisations[0][0].href);
+      expect(row1OdsCode.length).toEqual(1);
+      expect(row1OdsCode.text().trim()).toEqual(context.organisations[0][1].data);
+      expect(row2.length).toEqual(1);
+      expect(row2OrgName.length).toEqual(1);
+      expect(row2OrgName.text().trim()).toEqual(context.organisations[1][0].data);
+      expect(row2OrgName.attr('href')).toEqual(context.organisations[1][0].href);
+      expect(row2OdsCode.length).toEqual(1);
+      expect(row2OdsCode.text().trim()).toEqual(context.organisations[1][1].data);
     });
   }));
 });
