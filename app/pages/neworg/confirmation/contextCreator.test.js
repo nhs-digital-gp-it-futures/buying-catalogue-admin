ddca49aa1e1ba398a979dc6496107b5f8d1cac17
kat1906 <katie.armstrong@bjss.com>
2020-04-09 15:46:31 +0100

    add context creator for confirmation page

---
+++ app/pages/neworg/confirmation/contextCreator.test.js
@@ -0,0 +1,17 @@
+import { getContext } from './contextCreator';
+import manifest from './manifest.json';
+
+describe('getContext for create org comfirmation', () => {
+  it('should add manifest with parameters', () => {
+    const context = getContext({ orgName: 'An org name' });
+    expect(context.backLinkText).toEqual(manifest.backLinkText);
+    expect(context.description).toEqual(manifest.description);
+    expect(context.dataTestId).toEqual(manifest.dataTestId);
+    expect(context.title).toEqual('An org name created');
+  });
+
+  it('should construct backLink', () => {
+    const context = getContext({ orgName: 'An org name' });
+    expect(context.backLinkHref).toEqual('/organisations');
+  });
+});
