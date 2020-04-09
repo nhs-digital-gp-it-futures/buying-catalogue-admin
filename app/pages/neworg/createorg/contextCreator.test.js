2fe38de8eafc6a94f3d1ce466d37b7b38920ef25
kat1906 <katie.armstrong@bjss.com>
2020-04-09 12:27:27 +0100

    add createorg contextCreator

---
+++ app/pages/neworg/createorg/contextCreator.test.js
@@ -0,0 +1,23 @@
+import manifest from './manifest.json';
+import { getContext } from './contextCreator';
+
+// TODO: amend tests when api is hooked up to test for org details added
+describe('createorg contextCreator', () => {
+  describe('getContext', () => {
+    it('should return the contents of the manifest', () => {
+      const context = getContext({});
+      expect(context.title).toEqual(manifest.title);
+      expect(context.description).toEqual(manifest.description);
+      expect(context.organisationNameHeading).toEqual(manifest.organisationNameHeading);
+      expect(context.odsCodeHeading).toEqual(manifest.odsCodeHeading);
+      expect(context.addressHeading).toEqual(manifest.addressHeading);
+      expect(context.primaryRoleIdHeading).toEqual(manifest.primaryRoleIdHeading);
+      expect(context.selectOrgButtonText).toEqual(manifest.selectOrgButtonText);
+    });
+
+    it('should construct backLinkHref', () => {
+      const context = getContext();
+      expect(context.backLinkHref).toEqual('/organisations/find/select?ods=X01');
+    });
+  });
+});
