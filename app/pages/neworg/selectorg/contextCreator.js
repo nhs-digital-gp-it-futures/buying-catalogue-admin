64402dbf9dfef3b2cd9565d2d53590bb3b9c579f
kat1906 <katie.armstrong@bjss.com>
2020-04-09 10:38:47 +0100

    add context creator with hard coded data

---
+++ app/pages/neworg/selectorg/contextCreator.js
@@ -0,0 +1,12 @@
+import manifest from './manifest.json';
+import { baseUrl } from '../../../config';
+import { extractObjectValuesToArray } from '../../../helpers/contextCreatorHelper';
+// TODO: remove import below when api is hooked up
+import mockOrgDetails from '../../../test-utils/fixtures/organisationDetails.json';
+
+export const getContext = () => ({
+  ...manifest,
+  ...mockOrgDetails,
+  address: mockOrgDetails.address ? extractObjectValuesToArray(mockOrgDetails.address) : [],
+  backLinkHref: `${baseUrl}/organisations/find?ods=${mockOrgDetails.odsCode}`,
+});
