e2a289c31f476f5d6cc30f1c9ca1c2da16367405
kat1906 <katie.armstrong@bjss.com>
2020-04-10 12:01:09 +0100

    add test for csrf token field

--- app/pages/neworg/findorg/template.test.js
+++ app/pages/neworg/findorg/template.test.js
@@ -9,9 +9,14 @@ const setup = {
   },
 };
 
+const mockContext = {
+  ...context,
+  csrfToken: 'mock-csrf-token',
+};
+
 describe('add org page', () => {
   it('should render a backLink', createTestHarness(setup, (harness) => {
-    harness.request(context, ($) => {
+    harness.request(mockContext, ($) => {
       const backLink = $('[data-test-id="go-back-link"]');
       expect(backLink.length).toEqual(1);
       expect(backLink.text().trim()).toEqual('Back');
@@ -20,45 +25,54 @@ describe('add org page', () => {
   }));
 
   it('should render a title', createTestHarness(setup, (harness) => {
-    harness.request(context, ($) => {
+    harness.request(mockContext, ($) => {
       const title = $('h1[data-test-id="find-org-page-title"]');
       expect(title.length).toEqual(1);
-      expect(title.text().trim()).toEqual(context.title);
+      expect(title.text().trim()).toEqual(mockContext.title);
     });
   }));
 
   it('should render a description', createTestHarness(setup, (harness) => {
-    harness.request(context, ($) => {
+    harness.request(mockContext, ($) => {
       const description = $('[data-test-id="find-org-page-description"]');
       expect(description.length).toEqual(1);
-      expect(description.text().trim()).toEqual(context.description);
+      expect(description.text().trim()).toEqual(mockContext.description);
     });
   }));
 
   describe('form field', () => {
     it('should render a label', createTestHarness(setup, (harness) => {
-      harness.request(context, ($) => {
+      harness.request(mockContext, ($) => {
         const label = $('label');
-        expect(label[0].attribs.for).toEqual(context.questions[0].id);
-        expect(label[0].children[0].data.trim()).toEqual(context.questions[0].mainAdvice);
+        expect(label[0].attribs.for).toEqual(mockContext.questions[0].id);
+        expect(label[0].children[0].data.trim()).toEqual(mockContext.questions[0].mainAdvice);
       });
     }));
 
     it('should render a textField', createTestHarness(setup, (harness) => {
-      harness.request(context, ($) => {
+      harness.request(mockContext, ($) => {
         const input = $('input:not([name=_csrf])');
-        expect(input[0].attribs.id).toEqual(context.questions[0].id);
-        expect(input[0].attribs.name).toEqual(context.questions[0].id);
+        expect(input[0].attribs.id).toEqual(mockContext.questions[0].id);
+        expect(input[0].attribs.name).toEqual(mockContext.questions[0].id);
         expect(input[0].attribs.type).toEqual('text');
       });
     }));
   });
 
+  it('should render hidden input with csrf token', createTestHarness(setup, (harness) => {
+    harness.request(mockContext, ($) => {
+      const formElement = $('input[name=_csrf]');
+      expect(formElement.length).toEqual(1);
+      expect(formElement.attr('type')).toEqual('hidden');
+      expect(formElement.attr('value')).toEqual(mockContext.csrfToken);
+    });
+  }));
+
   it('should render a continue button', createTestHarness(setup, (harness) => {
-    harness.request(context, ($) => {
+    harness.request(mockContext, ($) => {
       const button = $('[data-test-id="continue-button"] button');
       expect(button.length).toEqual(1);
-      expect(button.text().trim()).toEqual(context.continueButtonText);
+      expect(button.text().trim()).toEqual(mockContext.continueButtonText);
     });
   }));
 });
