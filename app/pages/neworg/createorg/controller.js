873e2144cf4d6c54bb8431c214790ba5f95c9b08
kat1906 <katie.armstrong@bjss.com>
2020-04-10 13:31:02 +0100

    add api calls for create org page

--- app/pages/neworg/createorg/controller.js
+++ app/pages/neworg/createorg/controller.js
@@ -1,3 +1,35 @@
 import { getContext } from './contextCreator';
+import { getData, postData } from '../../../apiProvider';
+import { logger } from '../../../logger';
 
-export const getCreateOrgContext = async () => getContext();
+export const getCreateOrgContext = async ({ odsCode, accessToken }) => {
+  const org = await getData({ endpointLocator: 'getOrgByOdsCode', options: { odsCode }, accessToken });
+  if (org) {
+    logger.info(`Organisation with ${odsCode}: ${org.name} found`);
+    return getContext({ orgData: org });
+  }
+  throw new Error(`No organisation data returned for odsCode: ${odsCode}`);
+};
+
+export const postAddOrg = async ({ odsCode, data, accessToken }) => {
+  try {
+    const orgData = await getData({ endpointLocator: 'getOrgByOdsCode', options: { odsCode }, accessToken });
+    const response = await postData({
+      endpointLocator: 'postAddOrg',
+      body: {
+        ...orgData,
+        catalogueAgreementSigned: !!data.catalogueAgreementSigned,
+      },
+      accessToken,
+    });
+
+    if (response.data && response.data.errors) {
+      logger.info(`Errors returned: ${JSON.stringify(response.data.errors)}`);
+      return { success: false, errors: response.data.errors };
+    }
+    logger.info(`Organisation added: ${JSON.stringify(data)}`);
+    return { success: true, orgId: response.data.organisationId };
+  } catch (err) {
+    throw new Error(err.response.data);
+  }
+};
---
+++ app/pages/neworg/createorg/controller.test.js
@@ -0,0 +1,144 @@
+import { getCreateOrgContext, postAddOrg } from './controller';
+import * as apiProvider from '../../../apiProvider';
+import * as contextCreator from './contextCreator';
+
+jest.mock('../../../logger');
+jest.mock('../../../apiProvider', () => ({
+  getData: jest.fn(),
+  postData: jest.fn(),
+}));
+
+jest.mock('./contextCreator', () => ({
+  getContext: jest.fn(),
+}));
+
+const mockOrgData = {
+  organisationId: 'org1',
+  organisationName: 'Greater Manchester CCG',
+  odsCode: 'X01',
+  primaryRoleId: 'ID123',
+  address: {
+    line1: 'C/O NHS ENGLAND, 1W09, 1ST FLOOR',
+    line2: 'QUARRY HOUSE',
+    line3: 'QUARRY HILL',
+    line4: null,
+    town: 'LEEDS',
+    county: 'WEST YORKSHIRE',
+    postcode: 'LS2 7UE',
+    country: 'ENGLAND',
+  },
+  catalogueAgreementSigned: true,
+};
+
+describe('create org confirmation page controller', () => {
+  describe('getCreateOrgContext', () => {
+    const accessToken = 'access_token';
+    const odsCode = 'abc';
+
+    afterEach(() => {
+      apiProvider.getData.mockReset();
+      contextCreator.getContext.mockReset();
+    });
+
+    it('should call getData once with the correct params', async () => {
+      apiProvider.getData
+        .mockResolvedValueOnce(mockOrgData);
+      await getCreateOrgContext({ odsCode, accessToken });
+      expect(apiProvider.getData.mock.calls.length).toEqual(1);
+      expect(apiProvider.getData).toHaveBeenCalledWith({ endpointLocator: 'getOrgByOdsCode', options: { odsCode }, accessToken });
+    });
+
+    it('should call getContext with the correct params when user data is returned by the apiProvider', async () => {
+      apiProvider.getData
+        .mockResolvedValueOnce(mockOrgData);
+      contextCreator.getContext
+        .mockResolvedValueOnce();
+
+      await getCreateOrgContext({ odsCode, accessToken });
+
+      expect(contextCreator.getContext.mock.calls.length).toEqual(1);
+      expect(contextCreator.getContext).toHaveBeenCalledWith({ orgData: mockOrgData });
+    });
+
+    it('should throw an error when no user data is returned from the apiProvider', async () => {
+      apiProvider.getData
+        .mockResolvedValueOnce();
+      try {
+        await getCreateOrgContext({ odsCode, accessToken });
+      } catch (err) {
+        expect(err).toEqual(new Error('No organisation data returned for odsCode: abc'));
+      }
+    });
+  });
+
+  describe('postAddOrg', () => {
+    const accessToken = 'access_token';
+    const odsCode = 'abc';
+
+    afterEach(() => {
+      apiProvider.getData.mockReset();
+      apiProvider.postData.mockReset();
+    });
+
+    it('should call getData once with the correct params', async () => {
+      apiProvider.getData
+        .mockResolvedValueOnce(mockOrgData);
+      await getCreateOrgContext({ odsCode, accessToken });
+      expect(apiProvider.getData.mock.calls.length).toEqual(1);
+      expect(apiProvider.getData).toHaveBeenCalledWith({ endpointLocator: 'getOrgByOdsCode', options: { odsCode }, accessToken });
+    });
+
+    it('should call postData once with the correct params when catalogue agreement is signed', async () => {
+      apiProvider.getData
+        .mockResolvedValueOnce(mockOrgData);
+      apiProvider.postData
+        .mockResolvedValueOnce({ data: { organisationId: 'org1' } });
+
+      await postAddOrg({ odsCode, data: { odsCode, catalogueAgreementSigned: 'catalogueAgreementSigned' }, accessToken });
+
+      expect(apiProvider.getData.mock.calls.length).toEqual(1);
+      expect(apiProvider.getData).toHaveBeenCalledWith({ endpointLocator: 'getOrgByOdsCode', options: { odsCode }, accessToken });
+      expect(apiProvider.postData.mock.calls.length).toEqual(1);
+      expect(apiProvider.postData).toHaveBeenCalledWith({
+        endpointLocator: 'postAddOrg',
+        body: {
+          ...mockOrgData,
+          catalogueAgreementSigned: true,
+        },
+        accessToken,
+      });
+    });
+
+    it('should return success as true and the orgId if api request is successful', async () => {
+      apiProvider.postData
+        .mockResolvedValueOnce({ data: { organisationId: 'org1' } });
+
+      const response = await postAddOrg({ odsCode, data: { odsCode }, accessToken });
+
+      expect(response.success).toEqual(true);
+      expect(response.orgId).toEqual('org1');
+    });
+
+    it('should return success as false with errors if api request is unsuccessful', async () => {
+      const mockError = { error: 'error' };
+      apiProvider.postData
+        .mockResolvedValueOnce({ data: { errors: [mockError] } });
+
+      const response = await postAddOrg({ odsCode, data: { odsCode }, accessToken });
+
+      expect(response.success).toEqual(false);
+      expect(response.errors).toEqual([mockError]);
+    });
+
+    it('should throw an error if api request is unsuccessful', async () => {
+      apiProvider.postData
+        .mockRejectedValueOnce({ response: { status: 500, data: '500 response data' } });
+
+      try {
+        await postAddOrg({ odsCode, data: { odsCode }, accessToken });
+      } catch (err) {
+        expect(err).toEqual(new Error('500 response data'));
+      }
+    });
+  });
+});
