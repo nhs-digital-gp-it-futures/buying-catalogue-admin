2a396ca87221e082481c95b587d28aa9869dbc7c
kat1906 <katie.armstrong@bjss.com>
2020-04-10 13:36:02 +0100

    controller calls api

--- app/pages/neworg/selectorg/controller.js
+++ app/pages/neworg/selectorg/controller.js
@@ -1,3 +1,12 @@
 import { getContext } from './contextCreator';
+import { getData } from '../../../apiProvider';
+import { logger } from '../../../logger';
 
-export const getSelectOrgContext = async () => getContext();
+export const getSelectOrgContext = async ({ odsCode, accessToken }) => {
+  const org = await getData({ endpointLocator: 'getOrgByOdsCode', options: { odsCode }, accessToken });
+  if (org) {
+    logger.info(`Organisation with ${odsCode}: ${org.name} found`);
+    return getContext({ orgData: org });
+  }
+  throw new Error(`No organisation data returned for odsCode: ${odsCode}`);
+};
