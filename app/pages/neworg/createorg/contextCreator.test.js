6bc4afe15de80cbd77362ab5ff5f9c2fa7d30716
kat1906 <katie.armstrong@bjss.com>
2020-04-10 12:25:03 +0100

    remove mockData

--- app/pages/neworg/createorg/contextCreator.test.js
+++ app/pages/neworg/createorg/contextCreator.test.js
@@ -1,11 +1,29 @@
 import manifest from './manifest.json';
 import { getContext } from './contextCreator';
+import { extractObjectValuesToArray } from '../../../helpers/contextCreatorHelper';
 
-// TODO: amend tests when api is hooked up to test for org details added
+const mockOrgData = {
+  organisationId: 'org1',
+  organisationName: 'Greater Manchester CCG',
+  odsCode: 'X01',
+  primaryRoleId: 'ID123',
+  address: {
+    line1: 'C/O NHS ENGLAND, 1W09, 1ST FLOOR',
+    line2: 'QUARRY HOUSE',
+    line3: 'QUARRY HILL',
+    line4: null,
+    town: 'LEEDS',
+    county: 'WEST YORKSHIRE',
+    postcode: 'LS2 7UE',
+    country: 'ENGLAND',
+  },
+  catalogueAgreementSigned: true,
+};
+
 describe('createorg contextCreator', () => {
   describe('getContext', () => {
     it('should return the contents of the manifest', () => {
-      const context = getContext({});
+      const context = getContext({ orgData: mockOrgData });
       expect(context.title).toEqual(manifest.title);
       expect(context.description).toEqual(manifest.description);
       expect(context.organisationNameHeading).toEqual(manifest.organisationNameHeading);
@@ -15,8 +33,14 @@ describe('createorg contextCreator', () => {
       expect(context.selectOrgButtonText).toEqual(manifest.selectOrgButtonText);
     });
 
+    it('should formet the address', () => {
+      const formattedAddress = extractObjectValuesToArray(mockOrgData.address);
+      const context = getContext({ orgData: mockOrgData });
+      expect(context.address).toEqual(formattedAddress);
+    });
+
     it('should construct backLinkHref', () => {
-      const context = getContext();
+      const context = getContext({ orgData: mockOrgData });
       expect(context.backLinkHref).toEqual('/organisations/find/select?ods=X01');
     });
   });
