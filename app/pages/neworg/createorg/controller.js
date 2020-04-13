571e06495c6244f25975992ab35251f3088941bd
kat1906 <katie.armstrong@bjss.com>
2020-04-13 11:48:50 +0100

    change name to organisationName in logger

--- app/pages/neworg/createorg/controller.js
+++ app/pages/neworg/createorg/controller.js
@@ -5,7 +5,7 @@ import { logger } from '../../../logger';
 export const getCreateOrgContext = async ({ odsCode, accessToken }) => {
   const org = await getData({ endpointLocator: 'getOrgByOdsCode', options: { odsCode }, accessToken });
   if (org) {
-    logger.info(`Organisation with ${odsCode}: ${org.name} found`);
+    logger.info(`Organisation with ${odsCode}: ${org.organisationName} found`);
     return getContext({ orgData: org });
   }
   throw new Error(`No organisation data returned for odsCode: ${odsCode}`);
