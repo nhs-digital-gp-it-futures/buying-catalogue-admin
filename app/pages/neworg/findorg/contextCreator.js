63be8a1e30813fd999eafb90f4b4ff01ece74be4
kat1906 <katie.armstrong@bjss.com>
2020-04-13 11:51:50 +0100

    add code to add errors to context

--- app/pages/neworg/findorg/contextCreator.js
+++ app/pages/neworg/findorg/contextCreator.js
@@ -1,11 +1,22 @@
 import manifest from './manifest.json';
 import { baseUrl } from '../../../config';
 
-export const getContext = ({ odsCode }) => ({
+const getErrors = ({ error, errors }) => {
+  let message = 'Something went wrong';
+  if (error === '404') message = 'Not found';
+  if (error === '406') message = 'Not a buyer org';
+  errors.split('+').forEach((errId) => {
+    if (errId === 'OrganisationAlreadyExists') message = 'Org already exists';
+  });
+  return { message };
+};
+
+export const getContext = ({ odsCode, error, errors }) => ({
   ...manifest,
   questions: manifest.questions.map((question) => {
     const questionWithData = { ...question };
     if (odsCode && question.id === 'odsCode') questionWithData.data = odsCode;
+    if (error || errors) questionWithData.error = getErrors({ error, errors });
     return questionWithData;
   }),
   backLinkHref: `${baseUrl}/organisations`,
