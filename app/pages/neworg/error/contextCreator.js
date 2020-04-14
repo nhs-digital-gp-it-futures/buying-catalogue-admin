68f8ae0334b41b5c5053488b5187f357ab73ff76
kat1906 <katie.armstrong@bjss.com>
2020-04-14 14:22:43 +0100

    move string manipulation into contextCreator

--- app/pages/neworg/error/contextCreator.js
+++ app/pages/neworg/error/contextCreator.js
@@ -1,11 +1,11 @@
 import manifest from './manifest.json';
 import { baseUrl } from '../../../config';
 
-const getErrorMessage = (errors, odsCode) => (errors.includes('OrganisationAlreadyExists')
+const getErrorMessage = ({ errorsString, odsCode }) => (errorsString.split('+').includes('OrganisationAlreadyExists')
   ? `The organisation with ODS code ${odsCode} already exists.` : 'Something went wrong.');
 
-export const getContext = ({ errors, odsCode }) => ({
+export const getContext = params => ({
   ...manifest,
-  errorMessage: getErrorMessage(errors, odsCode),
+  errorMessage: getErrorMessage(params),
   backLinkHref: `${baseUrl}/organisations/find`,
 });
--- app/pages/neworg/error/contextCreator.test.js
+++ app/pages/neworg/error/contextCreator.test.js
@@ -6,34 +6,34 @@ const odsCode = 'abc';
 
 describe('getContext for create org comfirmation', () => {
   it('should add manifest with parameters', () => {
-    const context = getContext({ errors: 'OrganisationAlreadyExists+AnotherError', odsCode });
+    const context = getContext({ errorsString: 'OrganisationAlreadyExists+AnotherError', odsCode });
     expect(context.backLinkText).toEqual(manifest.backLinkText);
     expect(context.dataTestId).toEqual(manifest.dataTestId);
   });
 
   it('should construct backLink', () => {
-    const context = getContext({ errors: 'OrganisationAlreadyExists+AnotherError', odsCode });
+    const context = getContext({ errorsString: 'OrganisationAlreadyExists+AnotherError', odsCode });
     expect(context.backLinkHref).toEqual(`${baseUrl}/organisations/find`);
   });
 
   describe('errorMessage', () => {
     it('should construct correct errorMessage when only error is OrganisationAlreadyExists', () => {
-      const context = getContext({ errors: 'OrganisationAlreadyExists', odsCode });
+      const context = getContext({ errorsString: 'OrganisationAlreadyExists', odsCode });
       expect(context.errorMessage).toEqual(`The organisation with ODS code ${odsCode} already exists.`);
     });
 
     it('should construct correct errorMessage when errors includes OrganisationAlreadyExists', () => {
-      const context = getContext({ errors: 'OrganisationAlreadyExists+AnotherError', odsCode });
+      const context = getContext({ errorsString: 'OrganisationAlreadyExists+AnotherError', odsCode });
       expect(context.errorMessage).toEqual(`The organisation with ODS code ${odsCode} already exists.`);
     });
 
     it('should construct correct errorMessage when only error is not OrganisationAlreadyExists', () => {
-      const context = getContext({ errors: 'OneError', odsCode });
+      const context = getContext({ errorsString: 'OneError', odsCode });
       expect(context.errorMessage).toEqual('Something went wrong.');
     });
 
     it('should construct correct errorMessage when errors do not include OrganisationAlreadyExists', () => {
-      const context = getContext({ errors: 'OneError+AnotherError', odsCode });
+      const context = getContext({ errorsString: 'OneError+AnotherError', odsCode });
       expect(context.errorMessage).toEqual('Something went wrong.');
     });
   });
