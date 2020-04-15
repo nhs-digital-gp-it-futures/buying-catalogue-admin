d31726099f541d2f29dde72727c3bf9efdd201d7
kat1906 <katie.armstrong@bjss.com>
2020-04-15 13:49:09 +0100

    add function to check the ods code is valid before redirecting

--- app/pages/neworg/findorg/controller.js
+++ app/pages/neworg/findorg/controller.js
@@ -1,3 +1,17 @@
 import { getContext } from './contextCreator';
+import { getData } from '../../../apiProvider';
 
 export const getFindOrgContext = params => getContext(params);
+
+export const getFindOrgByOds = async ({ odsCode, accessToken }) => {
+  try {
+    await getData({ endpointLocator: 'getOrgByOdsCode', options: { odsCode }, accessToken });
+
+    return { success: true };
+  } catch (err) {
+    if (err.status === 404 || err.status === 406) {
+      return { success: false, errorStatus: err.status };
+    }
+    throw new Error(err);
+  }
+};
