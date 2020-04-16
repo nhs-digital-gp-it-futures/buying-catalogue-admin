8448da95d82cd4ede0328b2cf7c58bb36eee5322
Adrian Soomro <37105695+assoomro@users.noreply.github.com>
2020-04-16 13:43:50 +0100

    6260: Add PS equivalent for startRedis.sh (#72)
    6260: Improvement of startRedis.sh

--- startRedis.sh
+++ startRedis.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-result=$( docker inspect -f '{{.State.Running}}' bc-redis)
+result=$(docker inspect -f '{{.State.Running}}' bc-redis)
 
 if [ "$result" = true ]; then
   echo "Redis container is already running"
 else
   echo "Redis container not running"
-  docker rm bc-redis
-  docker run --name bc-redis -p 6379:6379 -d redis
+  docker run --rm --name bc-redis -p 6379:6379 -d redis > /dev/null
   echo "Redis container now running"
 fi
