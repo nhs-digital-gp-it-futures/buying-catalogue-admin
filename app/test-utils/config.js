32a154059a4837f457a259562c1c1ff265db4f16
kat1906 <katie.armstrong@bjss.com>
2020-04-21 10:13:33 +0100

    add identityApiLocalhost to testing config

--- app/test-utils/config.js
+++ app/test-utils/config.js
@@ -2,6 +2,8 @@ export const apiLocalhost = 'http://localhost:5100';
 
 export const organisationsApiLocalhost = 'http://localhost:8075';
 
+export const identityApiLocalhost = 'http://localhost:5102/identity';
+
 export const apiPath = '/api/v1';
 
 export const clientLocalhost = 'http://localhost:1234';
