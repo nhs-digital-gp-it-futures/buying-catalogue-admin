44ab9e99b9c17df4b88263c97e8625639465597d
kat1906 <katie.armstrong@bjss.com>
2020-04-21 11:29:41 +0100

    add href for change org link

--- app/pages/neworg/createorg/contextCreator.js
+++ app/pages/neworg/createorg/contextCreator.js
@@ -6,5 +6,6 @@ export const getContext = ({ orgData }) => ({
   ...manifest,
   ...orgData,
   address: orgData.address ? extractObjectValuesToArray(orgData.address) : [],
+  changeOrgLinkHref: `${baseUrl}/organisations/find?ods=${orgData.odsCode}`,
   backLinkHref: `${baseUrl}/organisations/find/select?ods=${orgData.odsCode}`,
 });
