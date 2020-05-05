fd8eebb42e43f40a94bb227f34ad04d742eb6a6d
Fawad Ali <alibombaye@gmail.com>
2020-05-05 14:50:18 +0100

    remove capitals

--- app/helpers/createAuthProvider.js
+++ app/helpers/createAuthProvider.js
@@ -5,7 +5,7 @@ export const createAuthProvider = ({ config }) => {
   const unauthenticatedError = new ErrorContext({
     status: 401,
     title: 'You\'re not authorised to view this page',
-    description: 'You must be logged in as an admin USER to access Buying Catalogue admin.',
+    description: 'You must be logged in as an admin user to access Buying Catalogue admin.',
     backLinkHref: config.publicBrowseBaseUrl,
   });
   const authProvider = new AuthProvider({
