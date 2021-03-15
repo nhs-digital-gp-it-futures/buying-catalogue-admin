8d91bb8f940dc9baa6b499b404003173c890cc31
KeithLyonMastek <79255462+KeithLyonMastek@users.noreply.github.com>
2021-03-15 15:48:02 +0000

    update npm bcl to 1.3, include ConfigHelper (#201)

--- app/server.js
+++ app/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const { ConfigHelper } = require('buying-catalogue-library');
 const config = require('./config');
 const { App } = require('./app');
 const { routes } = require('./routes');
@@ -9,7 +10,8 @@ const { createAuthProvider } = require('./helpers/createAuthProvider');
 (async () => {
   Object.keys(config).map((configKey) => {
     if (config[configKey]) {
-      logger.info(`${configKey} set to ${config[configKey]}`);
+      const value = ConfigHelper.getConfigKeyValue(configKey, config[configKey]);
+      logger.info(`${configKey} set to ${value}`);
     } else {
       logger.error(`${configKey} not set`);
     }
